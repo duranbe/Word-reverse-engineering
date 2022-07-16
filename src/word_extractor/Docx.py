e1f177a6653db7b84ab7b8ba5f59e4dcfe1c9d8d
Benoît Durand <benoit.durand.mail@gmail.com>
2022-07-17 00:26:08 +0200

    Add POC for core properties parsing

--- src/word_extractor/Docx.py
+++ src/word_extractor/Docx.py
@@ -1,22 +1,33 @@
 import os
 import zipfile
 from lxml import etree as ET
+from datetime import datetime
 
 
-WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+WORD_NAMESPACE = 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
+DCTERMS = "http://purl.org/dc/terms/"
+CP = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
 PARA = WORD_NAMESPACE + 'p'
 TEXT = WORD_NAMESPACE + 't'
 TABLE = WORD_NAMESPACE + 'tbl'
 ROW = WORD_NAMESPACE + 'tr'
 CELL = WORD_NAMESPACE + 'tc'
 SECTION = WORD_NAMESPACE + 'sectPr'
+LANG = WORD_NAMESPACE + 'lang'
+BODY = WORD_NAMESPACE + 'body'
 
+NS = {
+    "w": WORD_NAMESPACE,
+    "dcterms": DCTERMS,
+    "cp": CP
+}
 
 class Docx:
 
     def __init__(self,filename):
 
         self.filename = filename
+        self.created_datetime = None
     
     def unzip(self):
     
@@ -30,3 +41,10 @@ class Docx:
                     filepath = os.path.join(foldername, filename)
                     filepath_zip = filePath.replace('tmp\\','')
                     zipObj.write(filepath,filepath_zip)
+
+    def get_document_info(self):
+        tree = ET.parse("tmp/docProps/core.xml")
+        root = tree.getroot()
+        dcterms_created = root.findall(f'.//dcterms:created',NS)[0]
+        self.created_datetime = datetime.strptime(dcterms_created.text,"%Y-%m-%dT%H:%M:%SZ")
+        
