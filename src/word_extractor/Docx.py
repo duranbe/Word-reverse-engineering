286ae232f584e05f37792cc762ffafa00daaf2b1
Benoît Durand <benoit.durand.mail@gmail.com>
2022-07-17 19:36:43 +0200

    Update Namespace list

--- src/word_extractor/Docx.py
+++ src/word_extractor/Docx.py
@@ -2,27 +2,8 @@ import os
 import zipfile
 from lxml import etree as ET
 from datetime import datetime
+from Namespace import NS
 
-
-WORD_NAMESPACE = 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
-DCTERMS = "http://purl.org/dc/terms/"
-CP = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
-DC = "http://purl.org/dc/elements/1.1/"
-PARA = WORD_NAMESPACE + 'p'
-TEXT = WORD_NAMESPACE + 't'
-TABLE = WORD_NAMESPACE + 'tbl'
-ROW = WORD_NAMESPACE + 'tr'
-CELL = WORD_NAMESPACE + 'tc'
-SECTION = WORD_NAMESPACE + 'sectPr'
-LANG = WORD_NAMESPACE + 'lang'
-BODY = WORD_NAMESPACE + 'body'
-
-NS = {
-    "w": WORD_NAMESPACE,
-    "dcterms": DCTERMS,
-    "cp": CP,
-    "dc": DC
-}
 
 class Docx:
 
---
+++ src/word_extractor/Namespace.py
@@ -0,0 +1,22 @@
+WORD_NAMESPACE = 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'
+DCTERMS = "http://purl.org/dc/terms/"
+CP = "http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
+DC = "http://purl.org/dc/elements/1.1/"
+DCMITYPE = "http://purl.org/dc/dcmitype/"
+XSI = "http://www.w3.org/2001/XMLSchema-instance"
+PARA = WORD_NAMESPACE + 'p'
+TEXT = WORD_NAMESPACE + 't'
+TABLE = WORD_NAMESPACE + 'tbl'
+ROW = WORD_NAMESPACE + 'tr'
+CELL = WORD_NAMESPACE + 'tc'
+SECTION = WORD_NAMESPACE + 'sectPr'
+LANG = WORD_NAMESPACE + 'lang'
+BODY = WORD_NAMESPACE + 'body'
+
+NS = {
+    "w": WORD_NAMESPACE,
+    "dcterms": DCTERMS,
+    "cp": CP,
+    "dc": DC
+}
+
